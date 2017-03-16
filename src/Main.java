444ffa3d175479b0437c6a9aa74bfcf812af9778
Dr. Auxin <paul.torrillo@gmail.com>
2017-03-16 10:29:33 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] ranks = {"jack", "queen", "king"};
+
+        String[] suits = {"blue", "red"};
+
+        int[] pointValues = {11, 12, 13};
+
+        Deck d = new Deck(ranks, suits, pointValues);
+        System.out.println(d.size());
     }
 }
